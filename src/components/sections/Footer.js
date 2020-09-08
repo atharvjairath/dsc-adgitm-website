209e79b82b9531c056a4a242afb7b00f9774d768
ayanluap <ayanluap@gmail.com>
2020-09-08 20:54:26 +0530

    animations added

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <div className="footer u-margin-top-big ">
       <h1>Footer</h1>
     </div>
   );
