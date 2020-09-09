7b1d83b4ab748d081aa33be3781cf9b7e4a4e239
Atharv jairath <54663702+atharvjairath@users.noreply.github.com>
2020-09-09 18:32:12 +0530

    Update App.js
    added route to event

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import '../App.css';
 
 // IMPORTING PAGES
 import Home from './pages/Home';
+import Events from './pages/Eventpage';
 
 // IMPORTING LAYOUTS
 import Navbar from './layouts/Navbar';
@@ -20,7 +21,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/Blog" component={Home} />
-        <Route path="/Events" component={Home} />
+        <Route path="/Events" component={Events} />
         <Route path="/Team" component={Home} />
         <Route path="/Join" component={Home} />
         <Route path="/Projects" component={Home} />
