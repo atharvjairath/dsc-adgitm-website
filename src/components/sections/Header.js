71b32379a09e436d579da60e5d9aacafbcd3d5ba
ayanluap <ayanluap@gmail.com>
2020-09-10 16:01:26 +0530

    responsiveness added

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,21 +1,45 @@
-import React from 'react';
-import DscLogoV from '../../assets/dsc_logo_v.png';
+import React, { Fragment } from 'react';
+import { DownArrow, DscLogo, Think } from '../../assets/logo';
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="header__bg">
-        <div className="header__content u-text-center">
-          <img src={DscLogoV} alt="Logo" className="header__img" />
-          <div className="header__arrow-down">&darr;</div>
+    <Fragment>
+      <div className="header u-mb-big">
+        <div className="u-container">
+          <div className="header__content">
+            <div className="header__left">
+              <div className="header__logo u-mb-small">
+                <DscLogo />
+                <p className="text">Dsc adgitm</p>
+              </div>
+              <h3 className="header__head h2 u-mb-small">
+                Powered By Google Developers
+              </h3>
+              <p className="text">
+                DSC is a Google Developers program for University students,
+                designed to help you build your mobile and web development
+                skills and knowledge. <br />
+                <span className="extra">
+                  It is open to any student, ranging from novice (developers who
+                  are just starting), to advanced (developers who want to
+                  further improve their skills). Join Us!!
+                </span>
+              </p>
+            </div>
+            <div className="header__right">
+              <Think />
+            </div>
+          </div>
+          <div className="header__down-arrow">
+            <DownArrow />
+          </div>
         </div>
       </div>
-
       <div className="header__red-bubble"></div>
       <div className="header__blue-bubble"></div>
       <div className="header__green-bubble"></div>
       <div className="header__yellow-bubble"></div>
-    </div>
+    </Fragment>
   );
 };
 
