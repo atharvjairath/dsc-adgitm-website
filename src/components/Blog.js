2fff6722d6a5b4e1b087180f5b6569025b94961c
Uday Yadav <dev117uday@gmail.com>
2020-09-11 11:48:26 +0530

    bug fixes

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, makeStyles, Button, Container,} from '@material-ui/core'; 
+import { Grid, makeStyles, Button, Container, } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,115 +7,86 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Blogimage from '../assets/blog.jpg';
-// import className from 'className';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      maxWidth: 500,
-      // alignSelf:'center'
-      height:450,
-      marginBottom:"40px",
+        maxWidth: 500,
+        height: 440,
+        marginBottom: "40px",
     },
     media: {
-      paddingTop: '56.25%', // 16:9
+        paddingTop: '56.25%', // 16:9
     },
-    heading:{
-        fontFamily:'babel',
-        paddingTop:'50px',
-        paddingBottom:'50px',
+    heading: {
+        fontFamily: 'babel',
+        paddingTop: '50px',
+        paddingBottom: '50px',
     },
-    button:{
-        marginLeft:"45%",
-        marginRight:"45%",
-    },
-    center:{
-        margin:'auto',
+    center: {
+        margin: 'auto',
     },
     gridContainer: {
         paddingLeft: "40px",
         paddingRight: "40px",
     },
-  
-  }));
+    viewbutton: {
+        marginLeft: "39%",
+        marginRight: "40%"
+    }
+
+}));
 
 export default function Blog() {
-    const classes=useStyles();
-    // const cardclass = className("root","griditem");
+    const classes = useStyles();
     return (
         <div>
             <div className={classes.heading}>
-                <Typography variant="h2" align="center"> Recent Blogs</Typography>
-                <Button size='large' className={classes.button} link="http://medium.com/"> View on medium ></Button>
+                <Typography variant="h2" align="center">Blogs from DSC ADGTIM</Typography>
+                <br />
+                <div align="center">
+                    <Button size="large" variant="contained" color="primary" className={classes.button} link="http://medium.com/"> View on medium </Button>
+                </div>
             </div>
-            <br></br>
-            <br></br>
+            <br />
+            <br />
             <Container>
-            <Grid spacing={4} container className={classes.gridContainer} justify="center">
-                <Grid items xs={12} sm={6} >
-                    <Card className={classes.root} variant="outlined">
-                        <CardActionArea>
-                            <CardMedia
-                            component="img"
-                            alt="Contemplative Reptile"
-                            height="300"
-                            image={Blogimage}
-                            title="Contemplative Reptile"
-                            />
-                            <CardContent>
-                            <Typography gutterBottom variant="h3" component="h2">
-                                Blog title
-                            </Typography>
-                            <Typography variant="body" color="textSecondary" component="p">
-                                First Blog of DSc Adgitm Content of blog  Content of blog Content of blog Content of blog Content of blog Content of blog .....
-                            </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size="large" color="primary">
-                            Share
-                            </Button>
-                            <Button size="large" color="primary">
-                            Read More
-                            </Button>
-                        </CardActions>
-                    </Card>
+                <Grid spacing={4} container className={classes.gridContainer} justify="center">
+                    <Grid item xs={12} sm={6} >
+                        <Card className={classes.root} variant="outlined">
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    alt="Contemplative Reptile"
+                                    height="300"
+                                    image={Blogimage}
+                                    title="Contemplative Reptile"
+                                />
+                                <CardContent>
+                                    <Typography variant="h3" component="h2">
+                                        Coming Soon
+                                    </Typography>
+                                    <Typography variant="h5" color="textSecondary" component="p">
+                                        Stay tuned
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <div align="center">
+                                    <Button size="large" variant="contained" color="secondary" disabled>
+                                        Read More
+                                    </Button>
+                                </div>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+
+                    </Grid>
                 </Grid>
-                <Grid items xs={12} sm={6}>
-                <Card className={classes.root} variant="outlined">
-                        <CardActionArea>
-                            <CardMedia
-                            component="img"
-                            alt="Contemplative Reptile"
-                            height="300"
-                            image={Blogimage}
-                            title="Contemplative Reptile"
-                            />
-                            <CardContent>
-                            <Typography gutterBottom variant="h3" component="h2" link>
-                                Blog title
-                            </Typography>
-                            <Typography variant="body" color="textSecondary" component="p">
-                                Second Blog of DSC Adgitm. Content of blog  Content of blog Content of blog Content of blog Content of blog Content of blog
-                            </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size="large" color="primary">
-                            Share
-                            </Button>
-                            <Button size="large" color="primary">
-                             Read More
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid items xs={12} sm={6}>
-                    
-                </Grid>
-                <Grid items xs={12} sm={6}>
-                    
-                </Grid>
-            </Grid>
             </Container>
         </div>
     );
