d8216dcb2146372cce003e525c329fbbc1943790
dev117uday@gmail.com <dev117uday@gmail.com>
2020-09-16 16:52:48 +0530

    added ml bootcamp

--- src/components/sections/Events.js
+++ src/components/sections/Events.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import CardMedia from "@material-ui/core/CardMedia";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import mlBootcamp from "./assets/events/ml-bootcamp.jpg";
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -26,12 +27,13 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: theme.spacing(8),
   },
   card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
   },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
+  media: {
+    height: 280,
+    
   },
   cardContent: {
     flexGrow: 1,
@@ -49,23 +51,42 @@ export default function Album() {
 
   return (
     <React.Fragment>
-    <br /> <br /><br /><br /><br /><br />
-    
+      <br /> <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <CssBaseline />
       <main>
         {/* Hero unit */}
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
               DSC ADGITM EVENTS
             </Typography>
-            <Typography variant="h4" align="center" color="textSecondary" paragraph>
+            <Typography
+              variant="h4"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
               Hamare Events mast hote hai!
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
                 <Grid item>
-                  <Button variant="contained" size="large" color="primary" disabled>
+                  <Button
+                    href="https://www.youtube.com/channel/UCWSdOgrUHjM8RBmjxtQTG1A/"
+                    variant="contained"
+                    size="large"
+                    color="primary"
+                  >
                     Youtube Channel
                   </Button>
                 </Grid>
@@ -80,21 +101,38 @@ export default function Album() {
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card className={classes.card}>
                   <CardMedia
-                    className={classes.cardMedia}
-                    image=""
-                    title="Image title"
+                    className={classes.media}
+                    image={mlBootcamp}
+                    title="Bootcamp"
                   />
                   <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h4" component="h2">
-                      Coming Soon
+                    <Typography gutterBottom variant="h3" component="h2">
+                      𝐃𝐞𝐜𝐢𝐩𝐡𝐞𝐫 𝐃𝐞𝐞𝐩 𝐋𝐞𝐚𝐫𝐧𝐢𝐧𝐠
                     </Typography>
-                    <Typography variant="h5">
-                      Stay tuned !!
+                    <Typography variant="h6">
+                      This will be a course consisting of consolidated efforts
+                      and lessons that many 𝐭𝐞𝐜𝐡 𝐞𝐧𝐭𝐡𝐮𝐬𝐢𝐚𝐬𝐭𝐬 have learnt over
+                      the years and will be presented by individuals who have
+                      done Deep Learning courses of Andrew Ng and Fast ai, made
+                      awesome projects and are working on research papers.
                     </Typography>
                   </CardContent>
                   <CardActions>
-                    <Button size="large" color="primary" disabled>
-                      View
+                    <Button
+                      size="large"
+                      href="https://www.youtube.com/watch?v=i6N6Lgvjdrk&feature=youtu.be"
+                      variant="contained"
+                      color="primary"
+                    >
+                      View Trailer
+                    </Button>
+                    <Button
+                      size="large"
+                      href="https://www.youtube.com/watch?v=kTyGBn0TWxQ"
+                      variant="contained"
+                      color="secondary"
+                    >
+                      View Session
                     </Button>
                   </CardActions>
                 </Card>
